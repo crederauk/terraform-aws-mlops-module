1c75a677700f5c2898ac2f6547f2f6f2edf961f4
Daniel Lenton <daniel.lenton@credera.co.uk>
2023-11-17 11:38:52 +0000

    fix: give the statebucket role access to module registry bucket

--- host_bucket/main.tf
+++ host_bucket/main.tf
@@ -1,6 +1,28 @@
-# TODO: Configure to be private
 resource "aws_s3_bucket" "module-registry" {
   bucket = var.bucket_name
 }
 
-# TODO: Add IAM role that has permissions to upload to bucket only, to be assumed in pipeline.
+resource "aws_s3_bucket_policy" "allow_access_to_tfstate_role" {
+  bucket = aws_s3_bucket.module-registry.id
+  policy = data.aws_iam_policy_document.allow_access_to_tfstate_role.json
+}
+
+data "aws_iam_policy_document" "allow_access_to_tfstate_role" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::283211881821:role/TerraformNonprodTFStateAccess"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetBucketLocation",
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      aws_s3_bucket.module-registry.arn,
+      "${aws_s3_bucket.module-registry.arn}/*"
+    ]
+  }
+}
