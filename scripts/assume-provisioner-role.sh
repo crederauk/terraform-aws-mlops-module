ad325319093fb4501f122773c04b966ed11ff92e
Daniel Lenton <daniel.lenton@credera.co.uk>
2023-11-24 16:24:11 +0000

    test: pushing to shared module registry bucket

--- scripts/assume-provisioner-role.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
-$(aws sts assume-role \
---role-arn arn:aws:iam::135544376709:role/NonprodProvisionerRole \
---role-session-name moduleUploadSession \
---query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
---output text))
