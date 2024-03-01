b0fcd8a5b2bc8be81721f1858f97d1cb3a3568e0
Konrad Bachusz <konrad.bachusz@credera.co.uk>
2024-03-01 15:20:17 +0000

    fix(formatting): flake8 fixes

--- mlops_ml_models/transfom_data.py
+++ mlops_ml_models/transfom_data.py
@@ -32,9 +32,17 @@ def split_data(df: pd.DataFrame, shuffle: bool) -> pd.DataFrame:
         print(f"Error loading data: {e}")
 
 
-# TODO finish
 def preprocess_df(df, preprocessing_script_path):
-    """"""
+    """This is a placeholder function to import the preprocess_data function
+    if it has been uploaded into s3 when the preprocessing_script_path is provided by the user.
+    Args:
+    df:
+    preprocessing_script_path: Path to the data preprocessing script declared in user's repo
+
+    Returns:
+        df: dataframe that has been processed or unchanged depending
+        if the preprocessing_script_path has been provided
+    """
     if preprocessing_script_path:
         try:
             print("Loading file")
