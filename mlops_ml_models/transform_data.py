ce37d8477fd46f3909e1bd2b5bf1e900f5176423
AsranUzmen <asran.uzmen@credera.co.uk>
2024-05-09 11:25:41 +0100

    feat(transform_data): add functions for feature selection, and unit test

---
+++ mlops_ml_models/transform_data.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+from pycaret.classification import setup, get_config
+from sklearn.linear_model import RidgeClassifier
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+import matplotlib.pyplot as plt
+
+def split_data(df: pd.DataFrame, shuffle: bool) -> pd.DataFrame:
+    """This script split the data into test_data and train_data,
+    with optional shuffle function
+
+    Note:
+        Remember that this function returns 2 values, therefore using,
+        make sure you declare 2 variables using this function
+        ex.(train_data, test_data = split_data(df, shuffle=True))
+
+    Args:
+        df (pd.DataFrame): dataframe that you want to split
+        shuffle (boolean): If true, function will shuffle the dataframe,
+        else the order of data will remain the same
+
+    Returns:
+        pd.DataFrame: train_data
+        pf.DataFrame: test_data
+    """
+    try:
+        if shuffle:
+            df = df.sample(frac=1).reset_index(drop=True)
+
+        train_size = int(0.8 * len(df))
+        train_data = df[:train_size]
+        test_data = df[train_size:]
+
+        return train_data, test_data
+    except Exception as e:
+        print(f"Error loading data: {e}")
+
+
+def preprocess_df(df, preprocessing_script_path):
+    """This is a placeholder function to import the preprocess_data function
+    if it has been uploaded into s3 when the preprocessing_script_path is provided by the user.
+    Args:
+    df:
+    preprocessing_script_path: Path to the data preprocessing script declared in user's repo
+
+    Returns:
+        df: dataframe that has been processed or unchanged depending
+        if the preprocessing_script_path has been provided
+    """
+    if preprocessing_script_path:
+        try:
+            print("Loading file")
+            from preprocess_data import preprocess_data
+
+            df = preprocess_data(df)
+            assert isinstance(df, pd.DataFrame)
+        except ImportError:
+            print("File does not exist")
+    return df
+
+
+def one_hot_encode(data):
+    categorical_columns = data.select_dtypes(include=['object']).columns.tolist()
+
+    encoder = OneHotEncoder(sparse_output=False)
+
+    one_hot_encoded = encoder.fit_transform(data[categorical_columns])
+
+    one_hot_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(categorical_columns))
+
+    df_encoded = pd.concat([data, one_hot_df], axis=1)
+
+    df_encoded = df_encoded.drop(categorical_columns, axis=1)
+
+    return df_encoded, categorical_columns
+
+def normalise_data(data):
+    # Data normalisation
+    numeric_features = data.drop(columns=[target_variable]).select_dtypes(include=np.number).columns.tolist()
+
+    scaler = StandardScaler()
+    scaled_features = scaler.fit_transform(data[numeric_features])
+
+    scaled_data = pd.DataFrame(scaled_features, columns=numeric_features)
+    scaled_data = pd.concat([scaled_data, data[data.columns.difference(numeric_features)]], axis=1)
+
+    df_encoded, categorical_columns = one_hot_encode(scaled_data)
+
+    return df_encoded, categorical_columns
+
+def feature_selection(data, target_variable, features_to_keep=0.5):
+
+    processed_data, categorical_columns = normalise_data(data)
+
+    # Setup Ridge classifier
+    s = setup(data=processed_data, target=target_variable, feature_selection=False)
+
+    X_train = get_config('X_train')
+    y_train = get_config('y_train')
+
+    print("Getting feature importances...")
+    print("Creating Ridge model...")
+    model = RidgeClassifier()
+    model.fit(X_train, y_train)
+
+    # Extract feature importance dataframe
+    feature_importance = pd.DataFrame(np.abs(model.coef_[0]),
+                                      index=X_train.columns,
+                                      columns=['importance']).sort_values('importance', ascending=False)
+
+    # reverse the one hot encoding by averaging the importance for the encoded columns
+    for col in categorical_columns:
+        unique=data[col].unique()
+        tot_importance=0
+        for s in unique:
+            col_name=f"{col}_{s}"
+            col_importance=feature_importance['importance'][col_name]
+            print(f"{col_name}: {col_importance}")
+            tot_importance+=col_importance
+            feature_importance=feature_importance.drop(index=col_name)
+        avg_importance=tot_importance/len(unique)
+        print(f"Average importance for column {col} is {avg_importance}")
+        feature_importance.loc[col] = avg_importance
+
+    feature_importance=feature_importance.sort_values(by='importance',  ascending=False)
+    print('\n\nFeature importances:')
+    print(feature_importance)
+    print('\n')
+
+
+    if features_to_keep < 1:
+        features_to_keep = int(features_to_keep * len(feature_importance))
+    else:
+        features_to_keep = min(features_to_keep, len(feature_importance))
+
+    important_features = feature_importance[:features_to_keep].index.tolist()
+    discarded_features = feature_importance[features_to_keep:].index.tolist()
+
+    print(f"Keeping features: {important_features}\n")
+    print(f"Discarding features: {discarded_features}\n")
+
+    # Plot feature importances
+    plt.bar(important_features, feature_importance.loc[important_features, 'importance'], color='darkblue', label='Kept Features')
+    plt.bar(discarded_features, feature_importance.loc[discarded_features, 'importance'], color='grey', label='Discarded Features')
+    plt.xlabel('Features')
+    plt.ylabel('Importance')
+    plt.title('Feature Importances from Ridge Classifier')
+    plt.xticks(rotation=90)
+    plt.legend()
+    plt.show()
+
+    # Return modifed dataframe
+    data_important_features = data[important_features + [target_variable]]
+    return data_important_features
--- tests/test_transform_data.py
+++ tests/test_transform_data.py
@@ -1,4 +1,4 @@
-from mlops_ml_models.transfom_data import split_data, preprocess_df
+from mlops_ml_models.transform_data import split_data, preprocess_df, feature_selection
 import pandas as pd
 import pytest
 
@@ -20,6 +20,31 @@ def mock_df() -> pd.DataFrame:
             "col1": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
             "col2": ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A"],
             "col3": [4.5, 5.5, 6.5, 4.5, 5.5, 6.5, 4.5, 5.5, 6.5, 6.5],
+        }
+    )
+
+def mock_df_2() -> pd.DataFrame:
+    """This creates a mock dataframe based on the data
+    entered in the columns below. col1 is the 'feature', cols 2-5 are directly
+    related to col1 and so should have strong feature importance, cols 6-9 are
+    not related to col1 and so should have low feature importance
+
+    Returns:
+        pd.DataFrame: dataframe created from the script. Should be the same as
+        in the resources.
+    """
+    return pd.DataFrame(
+        {
+            "col1": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
+            "col2": ["A", "B", "C", "A", "B", "C", "A", "B", "C", "A"],
+            "col3": [2, 3, 4, 2, 3, 4, 2, 3, 4, 2],
+            "col4": [1, 4, 9, 1, 4, 9, 1, 4, 9, 1],
+            "col5": [1, 1.41, 1.73, 1, 1.41, 1.73, 1, 1.41, 1.73, 1],
+            "col6": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            "col7": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            "col8": [0.08, 0.21, 0.63, 0.32, 0.81, 0.73, 0.92, 0.39, 0.66, 0.51],
+            "col9": ["G", "W", "M", "Q", "S", "P", "G", "Z", "C", "A"],
+            "col1_again": [1, 2, 3, 1, 2, 3, 1, 2, 3, 1]
         }
     )
 
@@ -66,3 +91,15 @@ def test_preprocess_df_no_path(mock_df: pd.DataFrame) -> None:
     df = preprocess_df(mock_df, preprocessing_script_path)
 
     assert df.equals(mock_df)
+
+def test_feature_selection(mock_df_2: pd.DataFrame) -> None:
+    """This test checks if the feature selection function is able to only select the important features in the mock_df_2 dataframe.
+
+    Args:
+    mock_df_2: mock dataframe"""
+
+    df = feature_selection(mock_df_2)
+
+    non_important_cols = ['col6', 'col7', 'col8', 'col9']
+
+    assert not df.index.isin(non_important_cols).any()
